94b1546b59f6d88aa8521e58ca352b1945dd2dfe
AnthonyNgGitHub <ng_anthony@outlook.com>
2021-02-09 17:39:57 -0500

    Added RScripts for D2 and D3 along with screenshots

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,40 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance,data
 # Generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance,data=mechacar_data))
 
+# Import and read in the Suspension_Coil.csv file as a dataframe
+suspension_coil <- read.csv('data/Suspension_Coil.csv',check.names = F,stringsAsFactors = F) 
+
+# Summary statistics for all cars
+total_summary <- summarize(suspension_coil,
+                           Mean = mean(PSI, na.rm = TRUE),
+                           Median = median(PSI, na.rm=TRUE),
+                           Variance = var(PSI, na.rm = TRUE),
+                           SD = sd(PSI, na.rm = TRUE))
+
+# Group data by Manufacturing Lot
+by_lot <- group_by(suspension_coil,Manufacturing_Lot)
+
+# Summary statistics grouped by lot
+lot_summary <- summarize(by_lot,
+                           Mean = mean(PSI, na.rm = TRUE),
+                           Median = median(PSI, na.rm=TRUE),
+                           Variance = var(PSI, na.rm = TRUE),
+                           SD = sd(PSI, na.rm = TRUE))
+
+# Perform t-tests to determine if all manufacturing lots and each lot individually are statistically different 
+# from the population mean of 1,500 pounds per square inch.
+population_mean_PSI <- 1500
+?t.test()
+
+# 1. Determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(suspension_coil$PSI,mu=population_mean_PSI)
+
+# 2. Determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+
+# Create subsets of data for each lot
+subtset_Lot1 <- subset(suspension_coil, Manufacturing_Lot == "Lot1")
+subtset_Lot2 <- subset(suspension_coil, Manufacturing_Lot == "Lot2")
+subtset_Lot3 <- subset(suspension_coil, Manufacturing_Lot == "Lot3")
+
+# t-test on Lot1
+t.test(subtset_Lot1$PSI,mu=population_mean_PSI)
--- README.md
+++ README.md
@@ -7,3 +7,24 @@ A Multiple Linear Regression has been performed to predict the MPG based on 5 fa
 This model may not be predicting the MPG of MechaCar prototypes effectively. While the r-squared value (coefficient of determination) is 0.70, which suggests that 70% of the variance in MPG can be explained by the model, the model is also missing some key variables due to the fact that the intercept is statistically significant. This implies that there are other factors contributing to the variation of MPG which are not included in this model.
 
 <img src="images/D1_Multiple_Linear_Regression.PNG"></img>
+
+## Summary Statistics on Suspension Coils
+The design specifications for the MechaCar suspension coils dictate that the variance of the suspension coils must not exceed 100 pounds per square inch. Does the current manufacturing data meet this design specification for all manufacturing lots in total and each lot individually? Why or why not?
+
+<img src="images/D2_total_summary.PNG"></img>
+
+<img src="images/D2_lot_summary.PNG"></img>
+
+
+
+## T-Tests on Suspension Coils
+
+Assuming a significance level of 0.05, the calculated P-value is above the significance level. Therefore, we do not have sufficient evidence to reject the null hypothesis. The sample mean of 1,498.78 is statistically similar to the population mean of 1,500.
+<img src="images/D3_t-test-on-all-lots.PNG"></img>
+
+Assuming a significance level of 0.05, the calculated P-value for Lot 1, 2 and 3 are 1, 0.6072, and 0.04168, respectively. Therefore, the sample mean of Lot 1 (1,500.00) and Lot 2 (1,500.20) are statistically similar to the population mean of 1,500, whereas the sample mean of Lot 3 (1,496.14) is statistically different than the population mean of 1,500.
+
+<img src="images/D3_t-test-on-each-lot.PNG"></img>
+
+## Study Design: MechaCar vs Competition
+
